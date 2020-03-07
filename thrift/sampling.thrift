c8643c271ab3042f451db8bc42d479d7b229a1d3
Ning Zhang <ning@autonomic.ai>
2020-03-06 19:31:00 -0800

    add support for tail-based sampling

--- thrift/sampling.thrift
+++ thrift/sampling.thrift
@@ -18,7 +18,50 @@ namespace php Jaeger.Thrift.Agent
 namespace netcore Jaeger.Thrift.Agent
 namespace lua jaeger.thrift.agent
 
-enum SamplingStrategyType { PROBABILISTIC, RATE_LIMITING }
+enum SamplingStrategyType { PROBABILISTIC, RATE_LIMITING, TAIL_BASED }
+
+// Config holds the configuration for tail-based sampling.
+struct TailBasedSamplingStrategy {
+	// DecisionWait is the desired wait time from the arrival of the first span of
+	// trace until the decision about sampling it or not is evaluated.
+	1: required i16 decisionWait
+	// NumTraces is the number of traces kept on memory. Typically most of the data
+	// of a trace is released after a sampling decision is taken.
+	2: required i16 numTraces
+	// ExpectedNewTracesPerSec sets the expected number of new traces sending to the tail sampling processor
+	// per second. This helps with allocating data structures with closer to actual usage size.
+	3: optional i16 expectedNewTracesPerSec
+	// PolicyCfgs sets the tail-based sampling policy which makes a sampling decision
+	// for a given trace when requested.
+	4: required list<TailBasedSamplingPolicy> policies
+}
+
+struct TailBasedSamplingPolicy {
+    1: required string name
+    2: required TailBasedSamplingPolicyType type
+    3: optional NumericAttributeStrategy numericAttributeStrategy
+    4: optional StringAttributeStrategy stringAttributeStrategy
+}
+
+struct NumericAttributeStrategy {
+  // Tag that the filter is going to be matching against.
+  1: required string key
+  // MinValue is the minimum value of the attribute to be considered a match.
+  2: required i16 min
+  // MaxValue is the maximum value of the attribute to be considered a match.
+  3: required i16 max
+}
+
+// StringAttributeCfg holds the configurable settings to create a string attribute filter
+// sampling policy evaluator.
+struct StringAttributeStrategy {
+  // Tag that the filter is going to be matching against.
+  1: required string key
+  // Values is the set of attribute values that if any is equal to the actual attribute value to be considered a match.
+  2: required list<string> values
+}
+
+enum TailBasedSamplingPolicyType { ALWAYS_SAMPLE, NUMERIC_ATTRIBUTE, STRING_ATTRIBUTE }
 
 // ProbabilisticSamplingStrategy randomly samples a fixed percentage of all traces.
 struct ProbabilisticSamplingStrategy {
@@ -52,6 +95,7 @@ struct SamplingStrategyResponse {
     2: optional ProbabilisticSamplingStrategy probabilisticSampling
     3: optional RateLimitingSamplingStrategy rateLimitingSampling
     4: optional PerOperationSamplingStrategies operationSampling
+    5: optional TailBasedSamplingStrategy TailBasedSampling
 }
 
 service SamplingManager {
